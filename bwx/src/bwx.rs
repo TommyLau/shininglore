45ecd94478ccfa09ca305c91cb06f8f704ef59d1
Tommy Lau <tommy.lhg@gmail.com>
2022-03-12 18:50:55 +0800

    'HEAD' block parsing finished

--- bwx/src/bwx.rs
+++ bwx/src/bwx.rs
@@ -1,15 +1,26 @@
-use std::io::{self, Cursor, Read};
-use byteorder::ReadBytesExt;
+use std::io::{self, Cursor, Error, ErrorKind, Read};
+use byteorder::{LittleEndian, ReadBytesExt};
 use tracing::{debug, error, info, span, warn, Level, trace};
+use tracing::field::debug;
+
+enum SlData {
+    I16(i16),
+    I32(i32),
+    String(String),
+    BlockSizeNumber(i32, i32),
+}
 
 #[derive(Debug, Default)]
 /// A BWX class to handle ShiningLore BNX / PNX file
 pub struct BWX {
-    data: Cursor<Vec<u8>>,
+    content: Cursor<Vec<u8>>,
     size: i32,
     blocks: i32,
     pub pointer: usize,
+
 }
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 impl BWX {
     /// Returns a BWX with the given file name
@@ -29,46 +40,69 @@ impl BWX {
         BWX { ..Default::default() }
     }
 
-    pub fn load_from_file(&mut self, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
-        info!("Read from file: {filename}");
+    #[tracing::instrument(skip(self, filename))]
+    pub fn load_from_file(&mut self, filename: &str) -> Result<()> {
+        debug!(filename);
 
         let data = std::fs::read(filename)?;
-        self.data = Cursor::new(data);
-        //println!("{:?}", data);
+        self.content = Cursor::new(data);
 
         self.check_bwx_header()?;
-        self.data.set_position(4);
+        self.content.set_position(4);
 
-        self.size = self.read_i32_packed()?;
-        self.blocks = self.read_i32_packed()?;
-        info!(filename, self.size, blocks=self.blocks);
-        debug!("sz: {}, blocks: {}", self.size, self.blocks);
+        /*
+        (self.size, self.blocks) = match self.read_block_size_number()? {
+            SlData::BlockSizeNumber(size, blocks) => (size, blocks),
+            SlData::I32(_) => {}
+            SlData::String(_) => {}
+        };
+         */
+        (self.size, self.blocks) = self.read_block_size_number()?;
 
-        let name = self.read_string()?;
-        debug!("section name: {name}");
+        debug!(self.size, self.blocks);
 
-        info!("Pointer: {}", self.data.position());
+
+        for i in 0..self.blocks {
+            debug!("Block {i}");
+
+            let name = self.read_string()?;
+            match name.as_str() {
+                "0" => {
+                    // TODO: Add the data to Vector
+                    let _data = self.parse_block();
+                }
+                "HEAD" => {
+                    self.parse_head()?;
+                }
+                _ => {
+                    error!("Unhandled section: {name}");
+                    panic!("Unhandled section: {name}");
+                }
+            }
+
+            debug!("section name: {name}");
+        }
 
         Ok(())
     }
 
     /// Check whether the file is a valid BNX / PNX format
-    fn check_bwx_header(&mut self) -> Result<(), String> {
-        let header = &self.data.get_ref()[..4];
+    fn check_bwx_header(&mut self) -> Result<()> {
+        let header = &self.content.get_ref()[..4];
         if header != "BWXF".as_bytes() {
-            return Err("Invalid BWX file.".to_string());
+            return Err("Invalid BWX file.")?;
         }
 
         Ok(())
     }
 
     /// Read BNX / PNX special packed integer value (little endian)
-    fn read_i32_packed(&mut self) -> Result<i32, io::Error> {
+    fn read_i32_packed(&mut self) -> Result<i32> {
         let mut result: u32 = 0;
         let mut shift = 0;
 
         while shift < 35 {
-            let t = self.data.read_u8()? as u32;
+            let t = self.content.read_u8()? as u32;
             result |= (t & 0x7f) << shift;
 
             if t & 0x80 == 0 {
@@ -81,14 +115,70 @@ impl BWX {
         Ok(result as i32)
     }
 
+    /// Read block size & numbers
+    fn read_block_size_number(&mut self) -> Result<(i32, i32)> {
+        Ok((self.read_i32_packed()?, self.read_i32_packed()?))
+    }
+
     /// Read string
-    fn read_string(&mut self) -> Result<String, io::Error> {
-        let length = self.data.read_u8()?;
+    fn read_string(&mut self) -> Result<String> {
+        let length = self.content.read_u8()?;
         let mut buffer = Vec::new();
         buffer.resize(length as usize, 0);
-        self.data.read_exact(&mut buffer)?;
+        self.content.read_exact(&mut buffer)?;
 
         Ok(String::from_utf8_lossy(&buffer).trim_matches('\0').to_string())
+    }
+
+    ///
+    #[tracing::instrument(skip(self))]
+    fn parse_block(&mut self) -> Result<SlData> {
+        let sl_type = char::from(self.content.read_u8()?);
+
+        debug!("SL_TYPE: {sl_type}");
+        match sl_type {
+            'S' => {
+                let value = self.read_string()?;
+                debug!("String: {value}");
+                Ok(SlData::String(value))
+            }
+            'A' | 'D' => {
+                let (size, blocks) = self.read_block_size_number()?;
+                debug!("Block size and numbers: {size}, {blocks}");
+                Ok(SlData::BlockSizeNumber(size, blocks))
+            }
+            'I' => {
+                let value = self.content.read_i32::<LittleEndian>()?;
+                debug!("Integer: {value}");
+                Ok(SlData::I32(value))
+            }
+            'W' => {
+                let value = self.content.read_i16::<LittleEndian>()?;
+                debug!("Word: {value}");
+                Ok(SlData::I16(value))
+            }
+            _ => {
+                error!("Unhandled type {sl_type}");
+                panic!("Unhandled type {sl_type}");
+            }
+        }
+    }
+
+    /// Parse 'HEAD" block
+    #[tracing::instrument(skip(self))]
+    fn parse_head(&mut self) -> Result<()> {
+        let (size, mut blocks) = match self.parse_block()? {
+            SlData::BlockSizeNumber(s, b) => (s, b),
+            _ => (0, 0),
+        };
+        //let a=self.parse_block()?;
+        debug!("HEAD: size = {size}, blocks = {blocks}");
+        while blocks > 0 {
+            let _data = self.parse_block();
+            blocks -= 1;
+        }
+
+        Ok(())
     }
 }
 
@@ -99,9 +189,9 @@ mod tests {
     #[test]
     fn check_bwx_header() {
         let mut bwx = BWX { ..Default::default() };
-        bwx.data = Cursor::new(vec![66, 87, 88, 70]);
+        bwx.content = Cursor::new(vec![66, 87, 88, 70]);
         assert!(bwx.check_bwx_header().is_ok(), "File header check should pass");
-        bwx.data = Cursor::new(vec![11, 22, 33, 44]);
+        bwx.content = Cursor::new(vec![11, 22, 33, 44]);
         assert!(bwx.check_bwx_header().is_err(), "File header check should fail");
     }
 
@@ -109,9 +199,9 @@ mod tests {
     fn read_i32_packed() {
         let mut bwx = BWX { ..Default::default() };
         // Data from "EXTERNAL_UI_DEFAULT.PNX"
-        bwx.data = Cursor::new(vec![0xc1, 0xef, 0x5a, 0x0c]);
+        bwx.content = Cursor::new(vec![0xc1, 0xef, 0x5a, 0x0c]);
         assert_eq!(bwx.read_i32_packed().unwrap(), 1488833, "Packed integer value incorrect");
-        bwx.data = Cursor::new(vec![0x0c, 0x02]);
+        bwx.content = Cursor::new(vec![0x0c, 0x02]);
         assert_eq!(bwx.read_i32_packed().unwrap(), 12, "Packed integer value incorrect");
     }
 
@@ -119,7 +209,7 @@ mod tests {
     fn read_string() {
         let mut bwx = BWX { ..Default::default() };
         // Data from "EXTERNAL_UI_DEFAULT.PNX"
-        bwx.data = Cursor::new(vec![0x02, 0x30, 0x00, 0x53]);
+        bwx.content = Cursor::new(vec![0x02, 0x30, 0x00, 0x53]);
         assert_eq!(bwx.read_string().unwrap().as_str(), "0", "The string should be '0'");
     }
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -2,26 +2,19 @@ use bwx::BWX;
 use tracing::{error, Level, warn};
 use tracing_subscriber::{FmtSubscriber};
 
-fn main() {
-    #[cfg(debug_assertions)]
-    {
-        // a builder for `FmtSubscriber`.
-        let subscriber = FmtSubscriber::builder()
-            // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
-            // will be written to stdout.
-            .with_max_level(Level::TRACE)
-            // completes the builder.
-            .finish();
-        tracing::subscriber::set_global_default(subscriber)
-            .expect("setting default subscriber failed");
-    }
 
-    warn!("Hello, world!");
-    error!("Hello, world!");
+fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(if cfg!(debug_assertions) { Level::DEBUG } else { Level::INFO })
+        .init();
+
     let mut b = BWX::new();
     let c = b.load_from_file("EXTERNAL_UI_DEFAULT.PNX");
     match c {
         Ok(()) => eprintln!("OK!"),
         Err(e) => eprintln!("Error: {e}"),
     }
+
+
+
 }

7bcc250e7fb4d2712714e9af7dc90ab718c4548f
Tommy Lau <tommy.lhg@gmail.com>
2022-03-14 01:12:23 +0800

    Invite RUST_LOG=trace environment for better debugging

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,10 +1,22 @@
 use bwx::BWX;
 use tracing::Level;
+use tracing_subscriber::filter::EnvFilter;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::fmt()
-        .with_max_level(if cfg!(debug_assertions) { Level::DEBUG } else { Level::INFO })
-        .init();
+    if let Ok(filter) = EnvFilter::try_from_default_env() {
+        if cfg!(debug_assertions) {
+            tracing_subscriber::fmt().with_env_filter(filter).init();
+        } else {
+            match filter.to_string().as_str() {
+                "error" | "warn" | "info" => { tracing_subscriber::fmt().with_env_filter(filter).init(); }
+                _ => {}
+            }
+        }
+    } else {
+        tracing_subscriber::fmt()
+            .with_max_level(if cfg!(debug_assertions) { Level::DEBUG } else { Level::ERROR })
+            .init();
+    }
 
     let mut b = BWX::new();
     let _c = b.load_from_file("EXTERNAL_UI_DEFAULT.PNX")?;
